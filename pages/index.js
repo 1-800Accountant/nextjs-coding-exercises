de1683ff5254a3a337a8b594cf103556ced7330b
Jimmie Jackson <jjackson@1800accountant.com>
2021-01-21 13:42:40 -0800

    reset

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import Quotes from '../components/quotes/Quotes';
 import Results from '../components/results/Results';
 import styles from '../styles/Home.module.css';
@@ -18,6 +19,9 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.header}>
+        <Link href="/fizz-buzz">FizzBuzz</Link>
+      </div>
       <main className={styles.main}>
         <Quotes handleResults={handleResults} />
         <Results results={results} />
